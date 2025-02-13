c1b392abd289dbdb3de1c543054c9e85ed1fd7a7
Apagafuegos <csantos6952@ceuandalucia.es>
2025-02-13 16:20:37 +0100

    Cambios de imports

--- backend/argh-backend/src/main/java/com/arg/fct/model/TutorLaboral.java
+++ backend/argh-backend/src/main/java/com/arg/fct/model/TutorLaboral.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import lombok.Data;
 
 @Entity
 public class TutorLaboral {
